27539ddc30210673da7496092b75e474b12486cc
Kantavit <krite-18354@hotmail.com>
2022-12-24 16:23:56 +0700

    update README.md and other file

---
+++ src/api.js
@@ -0,0 +1,14 @@
+// GEO DB Cities API
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'API KEY HERE',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+// Open Weather API
+export const WEATHER_API_KEY = "API KEY HERE";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
